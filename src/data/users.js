edb3c251d6582f138a553516344edd313f609ed7
Ana Duran <ana.duran-b@mail.escuelaing.edu.co>
2025-02-17 12:56:22 -0500

    login con usuarios y endpoint protected funcional con token

---
+++ src/data/users.js
@@ -0,0 +1,6 @@
+let users = [
+    { email: "admin@example.com", name: "admin", rol: "admin" },
+    { email: "user@example.com", name: "user", rol: "user" },
+  ];
+
+module.exports = users;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 // Pasos para correr:  node src/index.js (en la terminal)
-
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv').config();
 const express = require('express');
 const app = express();
 const listViewRouter = require('./routes/list-view-router');
 const listEditRouter = require('./routes/list-edit-router');    
+
+let users = require('./data/users');
 
 const validMethods = ['GET', 'POST', 'PUT', 'DELETE'];
 
@@ -17,14 +20,54 @@ const validateMethod = (req, res, next) => {
 
 app.use(validateMethod); //middleware a nivel de aplicación
 
+const JWTValidation = (req, res, next) => {
+    const token = req.headers.authorization.split(' ')[1];
+    console.log('Token recibido: ', token);
+
+    if (!token) {
+        return res.status(401).json({ message: 'Token is required.' });
+    }
+
+    try {
+        const payload = jwt.verify(token, process.env.SECRET_KEY);
+        req.user = payload;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Invalid token.' });
+    }
+    
+};
+
 app.use(express.json()); // middleware para parsear el body de las peticiones, habilita la lectura de JSON en peticiones POST y PUT.
 
 // implementamos los routers
 app.use('/api', listViewRouter);
 app.use('/api', listEditRouter);
 
+app.post('/login', (req, res) => {    
+    const { email } = req.body;
+    console.log(email);
+    const user = users.find(user => user.email === email);
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid email.' });
+    }
+
+    // firmamos
+    const token = jwt.sign(
+        { email: user.email, name: user.name, rol: user.rol },
+        process.env.SECRET_KEY
+    );
+
+    res.json(token);
+});
+
+// Ruta protegida (/protected)
+app.get('/protected', JWTValidation, (req, res) => {
+    res.json({ message: 'You have access to this protected route.', user: req.user });
+});
+
 // configuración del servidor
-const PORT = 3000;
+const PORT = process.env.PORT || 35000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
